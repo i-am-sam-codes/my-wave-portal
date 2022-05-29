2209c615097370f83970e66da5c6eca6ed1ee2a6
i-am-sam-codes <sammmceja@gmail.com>
2022-05-28 23:33:54 -0700

    adds deploy

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,7 +8,7 @@ contract WavePortal {
     uint256 totalWaves;
 
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("wazzup, I am a contract and I am smart hehe");
     }
 
     function wave() public {
